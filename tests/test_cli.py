c02f3684e913e2febd42d88239c627b7c36fea36
Miroslav Bauer <mirekys@gmail.com>
2024-02-05 18:50:38 +0100

    improve on config path handing, pass tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -5,7 +5,7 @@ from oarepo_tools.make_translations import main
 
 
 def test_cli_with_oarepo_yaml(app, db, cache, extra_entry_points, cli_runner):
-    config_file = Path(__file__).parent / "oarepo.yaml"
+    config_file = Path(__file__).parent
     try:
         cli_runner(main([str(config_file)]))
     except SystemExit as se:
@@ -13,7 +13,7 @@ def test_cli_with_oarepo_yaml(app, db, cache, extra_entry_points, cli_runner):
 
 
 def test_cli_with_setup_cfg(app, db, cache, extra_entry_points, cli_runner):
-    config_file = Path(__file__).parent / "setup.cfg"
+    config_file = Path(__file__).parent
     try:
         cli_runner(main([str(config_file)]))
     except SystemExit as se:
@@ -32,8 +32,18 @@ def test_cli_with_config_autodetect(app, db, cache, extra_entry_points, cli_runn
 
 
 def test_cli_with_empty_setupcfg(app, db, cache, extra_entry_points, cli_runner):
-    config_file = Path(__file__).parent / "empty_setup.cfg"
+    base_dir = Path(__file__).parent
+    empty_config_file = base_dir / "empty_setup.cfg"
+    real_config_file = base_dir / "setup.cfg"
+    backup_config_file = base_dir / "setup.cfg.bak"
+
+    real_config_file.rename(base_dir / backup_config_file)
+    empty_config_file.rename(real_config_file)
+
     try:
-        cli_runner(main([str(config_file)]))
+        cli_runner(main([]))
     except SystemExit as se:
         assert se.code == 0
+    finally:
+        real_config_file.rename(empty_config_file)
+        backup_config_file.rename(real_config_file)
